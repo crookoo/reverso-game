e9ffc34514fec86b7abe18ad6b949bf9647e22ec
Michael Zimmermann <info@michaelzimmermann.com>
2022-06-02 21:19:17 +0200

    Add webpack-pwa-manifest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Reverso Game
 
 A strategic game inspired by the famous [board game](https://en.wikipedia.org/wiki/Reversi).
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/84683dc3-79e6-42e8-b9de-6fc1a331ff80/deploy-status)](https://app.netlify.com/sites/reverso-game/deploys)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -17,6 +18,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'script.js',
+        publicPath: "",
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -28,5 +30,21 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'styles.css'
         }),
+        new WebpackPwaManifest({
+            name: 'Reverso Game',
+            short_name: 'Reverso',
+            description: 'A strategic game inspired by the famous board game.',
+            background_color: '#FFFFFF',
+            theme_color: '#cc2000',
+            lang: 'en',
+            filename: './icons/manifest.json',
+            icons: [
+              {
+                src: path.resolve(__dirname, 'src/favicon.png'),
+                sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+                destination: 'icons',
+              }
+            ],
+          })
     ]
 }
